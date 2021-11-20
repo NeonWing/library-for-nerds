8aa71d9f7f82c86efa5b21f518858f0458dc1abb
neonwing <neonwing3@gmail.com>
2021-11-20 13:30:20 -0500

    Added modal functionality

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ function addToLibrary(title, author, pages, hasRead) {
     let book = new Book(title, author, pages, hasRead);
     myLibrary.push(book);
 }
+
+//Modal Handling
+const modal = document.querySelector("#modal");
+const modalClose = document.querySelector(".close");
+const addButton = document.querySelector(".add");
+
+modalClose.addEventListener("click", () => {
+    modal.classList.toggle("closed");
+});
+
+addButton.addEventListener("click", () => {
+    modal.classList.toggle("closed");
+});
